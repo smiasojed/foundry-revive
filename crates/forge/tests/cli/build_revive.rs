26ac5cc17bc65dfdfffe32bd5dec35726c6d16d9
Siphamandla Mjoli <siphamandla@parity.io>
2025-02-25 02:22:42 +0200

    fix: remove unused test

--- crates/forge/tests/cli/build_revive.rs
+++ crates/forge/tests/cli/build_revive.rs
@@ -12,10 +12,3 @@ forgetest_init!(force_buid_with_revive, |prj, cmd| {
     prj.write_config(Config { bytecode_hash: BytecodeHash::None, ..Default::default() });
     cmd.args(["build", "--revive-compile", "--force"]).assert_success();
 });
-/* This test wont work as expected since revive produces much larger bytecode than solc
-forgetest!(initcode_size_limit_can_be_ignored_revive, |prj, cmd| {
-    prj.write_config(Config { bytecode_hash: BytecodeHash::None, ..Default::default() });
-    prj.add_source("LargeContract", generate_large_init_contract(50_000).as_str()).unwrap();
-    cmd.args(["build", "--revive-compile", "--sizes", "--ignore-eip-3860"]).assert_success();
-});
-*/
